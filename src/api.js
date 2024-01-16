b867933a6835061e712bfa945a6f227e60a77746
RatKingSaint <kdbeuthin@gmail.com>
2024-01-16 11:20:02 +0200

    init

---
+++ src/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:9000/api';
+
+export const getTestData = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/test`);
+    return response.data;
+  } catch (error) {
+    console.error('Error in getTestData:', error);
+    throw error;
+  }
+};
+
+module.exports=getTestData
